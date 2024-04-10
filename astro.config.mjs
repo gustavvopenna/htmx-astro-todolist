9add00db2c3d088b0af6d01b6908031dfea809cf
Gustavo Peña <gustavo.pema@gmail.com>
2024-04-09 19:53:20 -0600

    feat: add fly.io config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import db from "@astrojs/db";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind(), db()]
+  integrations: [tailwind(), db()],
+  adapter: node({
+    mode: "standalone"
+  })
 });
