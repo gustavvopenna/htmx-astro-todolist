74c3ff3353824698cd9121c1496bdf2094b40067
Gustavo Peña <gustavo.pema@gmail.com>
2024-04-11 20:47:37 -0600

    chore: change seed todos id

--- db/seed.ts
+++ db/seed.ts
@@ -3,8 +3,8 @@ import { db, Todo } from 'astro:db';
 // https://astro.build/db/seed
 export default async function seed() {
 	await db.insert(Todo).values([
-		{ id: '1', text: 'Learn Astro', completed: false },
-		{ id: '2', text: 'Build a project', completed: false },
-		{ id: '3', text: 'Deploy to the web', completed: false },
+		{ id: 'a1-2334-asdsa', text: 'Learn Astro', completed: false },
+		{ id: 'b2-3434-sdpo', text: 'Build a project', completed: false },
+		{ id: 'v3-3434-psaw', text: 'Deploy to the web', completed: false },
 	])
 }
