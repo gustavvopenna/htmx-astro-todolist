1949ec16ffa95e5381b98bdb799dfd52f3fe1c41
Gustavo Peña <gustavo.pema@gmail.com>
2024-04-13 15:16:32 -0600

    chore: add eslint and prettier

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,7 @@
 /// <reference path="../.astro/db-types.d.ts" />
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface Window {
+  Alpine: import('alpinejs').Alpine;
+}
